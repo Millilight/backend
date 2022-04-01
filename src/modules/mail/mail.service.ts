e0af452e73eefc2e7bb736a15e80d8014362abe2
Louis ROB <57103034+lperrious@users.noreply.github.com>
2022-04-01 15:20:35 +0200

    fix: add user_id in the frontend url form signup mail

--- src/modules/mail/mail.service.ts
+++ src/modules/mail/mail.service.ts
@@ -1,6 +1,6 @@
-import { MailerService } from '@nestjs-modules/mailer';
-import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { Injectable } from '@nestjs/common';
+import { MailerService } from '@nestjs-modules/mailer';
 import { User } from '../users/schemas/user.schema';
 
 @Injectable()
@@ -8,7 +8,7 @@ export class MailService {
   constructor(private mailerService: MailerService, private configService : ConfigService) {}
 
   async sendUserConfirmation(user: User, token: string) {
-    const url = `${this.configService.get<string>('baseUrls.front')}/auth/confirmation?token=${token}`;
+    const url = `${this.configService.get<string>('baseUrls.front')}/auth/confirmation?token=${token}&user_id=${user._id}`;
 
     await this.mailerService.sendMail({
       to: user.email,
