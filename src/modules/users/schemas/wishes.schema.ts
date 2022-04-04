3baa96df2e7f896b399874c0df584dfb435e2128
camillepltr <camille.peltier04@gmail.com>
2022-04-04 13:22:00 +0200

    feat: added music in Wishes

--- src/modules/users/schemas/wishes.schema.ts
+++ src/modules/users/schemas/wishes.schema.ts
@@ -1,20 +1,24 @@
-import { Field, ObjectType } from "@nestjs/graphql";
-import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Field, ObjectType } from '@nestjs/graphql';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 
-import { Document } from "mongoose";
+import { Document } from 'mongoose';
 
 export type WishesDocument = Wishes & Document;
 
 @Schema()
 @ObjectType()
 export class Wishes {
-  @Prop({type : String})
-  @Field(() => String, {nullable:true})
+  @Prop({ type: String })
+  @Field(() => String, { nullable: true })
   burial_cremation: string;
 
-  @Prop({type : String})
-  @Field(() => String, {nullable:true})
+  @Prop({ type: String })
+  @Field(() => String, { nullable: true })
   burial_cremation_place: string;
+
+  @Prop({ type: String })
+  @Field(() => String, { nullable: true })
+  music: string;
 }
 
 export const WishesSchema = SchemaFactory.createForClass(Wishes);
