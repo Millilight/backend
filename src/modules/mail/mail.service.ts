f22f64ee891a786e86d21d913c10289a11868f6a
Louis ROB <57103034+lperrious@users.noreply.github.com>
2022-04-04 14:16:56 +0200

    feat: update user's email

--- src/modules/mail/mail.service.ts
+++ src/modules/mail/mail.service.ts
@@ -26,14 +26,36 @@ export class MailService {
     });
   }
 
-  async resetPasswordEmail(user: User) {
-    const url = `${this.configService.get<string>('base_urls.front')}/auth/reinitialisation?token=${user.reset_password_token}&user_id=${user._id}`;
+  async resetPassword(user: User) {
+    const url = `${this.configService.get<string>('base_urls.front')}/user/reinitialisation?token=${user.reset_password_token}&user_id=${user._id}`;
 
     await this.mailerService.sendMail({
       to: user.email,
       // from: '"Support Team" <support@example.com>', // override default from
       subject: 'Merci de votre confiance',
       template: __dirname + '/resetPassword', // `.hbs` extension is appended automatically
+      context: { // filling curly brackets with content
+        firstname: user.firstname,
+        url,
+        email: user.email,
+        product_name: this.configService.get<string>('product_name'),
+        home_url: this.configService.get<string>('base_urls.home'),
+        front_url: this.configService.get<string>('base_urls.front')
+      },
+    })
+    .catch((err) => {
+      throw err;
+    });
+  }
+
+  async sendUserEmailUpdate(user: User) {
+    const url = `${this.configService.get<string>('base_urls.front')}/user/changementMail?token=${user.new_email_token}&user_id=${user._id}`;
+
+    await this.mailerService.sendMail({
+      to: user.new_email.toString(),
+      // from: '"Support Team" <support@example.com>', // override default from
+      subject: 'Merci de votre confiance',
+      template: __dirname + '/updateMail', // `.hbs` extension is appended automatically
       context: { // filling curly brackets with content
         firstname: user.firstname,
         url,
