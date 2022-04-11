d22ce54d90909b40eb9db29e05138d6b42013153
Quentin de Andria <contact@quentindeandria.com>
2022-04-11 15:36:01 +0200

    feat: more wishes

--- src/modules/users/schemas/wishes.schema.ts
+++ src/modules/users/schemas/wishes.schema.ts
@@ -1,30 +1,43 @@
-import { Field, ObjectType } from '@nestjs/graphql';
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 
 import { Document } from 'mongoose';
 
-export type WishesDocument = Wishes & Document;
+export type WishesDocument = WishesDB & Document;
 
-@Schema()
-@ObjectType()
-export class Wishes {
+@Schema({ collection: 'wishes' })
+export class WishesDB {
   @Prop({ type: String })
-  @Field(() => String, { nullable: true })
   burial_cremation?: string;
 
   @Prop({ type: String })
-  @Field(() => String, { nullable: true })
   burial_cremation_place?: string;
 
   @Prop({ type: String })
-  @Field(() => String, { nullable: true })
   music?: string;
+
+  @Prop({ type: String })
+  religion?: string;
+
+  @Prop({ type: String })
+  place?: string;
+
+  @Prop({ type: String })
+  prevoyance?: string;
+
+  @Prop({ type: String })
+  list_of_people?: string;
+
+  @Prop({ type: String })
+  coffin?: string;
+
+  @Prop({ type: String })
+  ornament?: string;
+
+  @Prop({ type: String })
+  text?: string;
+
+  @Prop({ type: String })
+  other?: string;
 }
 
-export const WishesSchema = SchemaFactory.createForClass(Wishes);
-
-@ObjectType()
-export class UrgentData {
-  @Field(() => Wishes)
-  wishes: Wishes;
-}
+export const WishesDBSchema = SchemaFactory.createForClass(WishesDB);
